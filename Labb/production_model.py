49ec78e343012969ff6d390f78307b411283a9aa
Daniel Nilsson <daniel.nilsson1@iths.se>
2023-03-24 11:24:38 +0100

    almost done with 2.8

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import joblib
 
 # import test_samples
-test_samples = pd.read_csv('../Labb/test_samples.csv')
+test_samples = pd.read_csv('../test_samples.csv')
 
 # import model
-from sklearn.externals import joblib
 model = joblib.load('svm_model.pkl')
 
 # make a prediction on the 100 test samples with the model
