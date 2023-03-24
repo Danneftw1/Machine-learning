5d44cddb338131343054f676cc919e5937f1b6e9
Daniel Nilsson <daniel.nilsson1@iths.se>
2023-03-24 16:47:33 +0100

    tried more for 2.8, not working

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -7,20 +7,16 @@ test_samples = pd.read_csv("Labb/test_samples.csv")
 # import model
 model = joblib.load('Labb/rf_model.pkl')
 
-# choose X and y to predict on test_samples
-X = test_samples.drop(columns=['cardio'])
-y = test_samples['cardio']
-
-# predict on test_samples
-y_pred = model.predict(X)
+# use trained model to predict on test_samples
+y_pred = model.predict(test_samples)
 
 # print accuracy score
-print("Accuracy score: ", model.score(X, y))
+print("Accuracy score: ", model.score(test_samples, y_pred))
 
 # export the predictions to a csv file called "predictions.csv" and it should have the following columns: "Probability class 0", "Probability class 1", "Prediction"
 
 # create a dataframe with the predictions
-predictions = pd.DataFrame(model.predict_proba(X), columns=['Probability class 0', 'Probability class 1'])
+predictions = pd.DataFrame(model.predict_proba(test_samples), columns=['Probability class 0', 'Probability class 1'])
 predictions['Prediction'] = y_pred
 
 # export the predictions to a csv file
