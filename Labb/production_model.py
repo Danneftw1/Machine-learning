df7ac13c994021c37e5fc9c69cb4ec64c5226acf
Daniel Nilsson <daniel.nilsson1@iths.se>
2023-03-24 13:23:55 +0100

    something wrong with model, predicts 1 on all rows

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -5,7 +5,7 @@ import joblib
 test_samples = pd.read_csv("Labb/test_samples.csv")
 
 # import model
-model = joblib.load('Labb/svm_model.pkl')
+model = joblib.load('Labb/rf_model.pkl')
 
 # choose X and y to predict on test_samples
 X = test_samples.drop(columns=['cardio'])
@@ -16,3 +16,14 @@ y_pred = model.predict(X)
 
 # print accuracy score
 print("Accuracy score: ", model.score(X, y))
+
+# export the predictions to a csv file called "predictions.csv" and it should have the following columns: "Probability class 0", "Probability class 1", "Prediction"
+
+# create a dataframe with the predictions
+predictions = pd.DataFrame(model.predict_proba(X), columns=['Probability class 0', 'Probability class 1'])
+predictions['Prediction'] = y_pred
+
+# export the predictions to a csv file
+predictions.to_csv('Labb/predictions.csv', index=False)
+
+
