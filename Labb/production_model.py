e07675b83dfa0c5831ce92bc8cdfcda63b14b5af
Daniel Nilsson <daniel.nilsson1@iths.se>
2023-03-25 14:49:06 +0100

    not sure what's wrong with .py file

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -8,12 +8,12 @@ test_samples = pd.read_csv("Labb/test_samples.csv")
 model = joblib.load('Labb/rf_model.pkl')
 
 # use trained model to predict on test_samples
-y_pred = model.predict(test_samples)
+y = test_samples['cardio']
+
+y_pred = model.predict_proba(y)
 
 # print accuracy score
 print("Accuracy score: ", model.score(test_samples, y_pred))
-
-# export the predictions to a csv file called "predictions.csv" and it should have the following columns: "Probability class 0", "Probability class 1", "Prediction"
 
 # create a dataframe with the predictions
 predictions = pd.DataFrame(model.predict_proba(test_samples), columns=['Probability class 0', 'Probability class 1'])
