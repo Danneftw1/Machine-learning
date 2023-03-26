098e1e7460079558ed9c6358fa42ebc24241a238
Daniel Nilsson <daniel.nilsson1@iths.se>
2023-03-26 13:58:55 +0200

    fixed last exercise for labb 2, now works correctl

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -8,16 +8,17 @@ test_samples = pd.read_csv("Labb/test_samples.csv")
 model = joblib.load('Labb/rf_model.pkl')
 
 # use trained model to predict on test_samples
-y = test_samples['cardio']
+X_test = test_samples.drop('cardio', axis=1)
+y_test = test_samples['cardio']
 
-y_pred = model.predict_proba(y)
+y_pred = model.predict_proba(X_test)
 
 # print accuracy score
-print("Accuracy score: ", model.score(test_samples, y_pred))
+print("Accuracy score: ", model.score(X_test, y_test))
 
 # create a dataframe with the predictions
-predictions = pd.DataFrame(model.predict_proba(test_samples), columns=['Probability class 0', 'Probability class 1'])
-predictions['Prediction'] = y_pred
+predictions = pd.DataFrame(y_pred, columns=['Probability class 0', 'Probability class 1'])
+predictions['Prediction'] = model.predict(X_test)
 
 # export the predictions to a csv file
 predictions.to_csv('Labb/predictions.csv', index=False)
